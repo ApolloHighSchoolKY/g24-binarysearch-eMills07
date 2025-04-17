8b8e18ad985e25a5063b9097d955710ec80e503b
eMills07 <elijah.mills2@stu.daviess.kyschools.us>
2025-04-17 04:48:19 +0000

    It's midnight gimme a break :(

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,28 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
+		
+		int low = 0;
+		int high = ray.length - 1;
+		int mid = 0;
+		
+		while(low <= high)
+		{
+			mid = low + (high - low)/2;
 
+			if(ray[mid] < item)
+			{
+				low = mid;
+			}
+			else if(ray[mid] == item)
+			{
+				return mid;
+			}
+			else
+			{
+				high = mid;
+			}
+		}
+	return mid;
 	}
 }
--- BinarySearchTester.java
+++ BinarySearchTester.java
@@ -5,6 +5,7 @@ public class BinarySearchTester
    public static void main( String args[] )
    {
 		int[] array = {39,6,11,23,18,3,20,5,57,15,8};
+		Arrays.sort(array);
 
 		System.out.println("before calling binarySearch");
 		System.out.println(Arrays.toString(array));
@@ -12,7 +13,7 @@ public class BinarySearchTester
 		System.out.println("looking for 8 :: "+	BinarySearch.binarySearch(array,8));
 		System.out.println("looking for 39 :: "+	BinarySearch.binarySearch(array,39));
 		System.out.println("looking for 15 :: "+	BinarySearch.binarySearch(array,15));
-		System.out.println("looking for 5 :: "+	BinarySearch.binarySearch(array,5);
-		//System.out.println("looking for 10 :: "+ BinarySearch.binarySearch(array,10);
+		System.out.println("looking for 5 :: "+	BinarySearch.binarySearch(array,5));
+		//System.out.println("looking for 10 :: "+ BinarySearch.binarySearch(array,10));
 	}
 }
